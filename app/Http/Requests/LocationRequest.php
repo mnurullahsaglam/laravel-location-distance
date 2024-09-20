5caefd433df119d3c8c210cc99356cba2da26f37
mnurullahsaglam <nurullahsl87@gmail.com>
2024-09-20 03:45:24 +0300

    create location form request

---
+++ app/Http/Requests/LocationRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LocationRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', 'max:255', 'unique:locations,name,' . $this->location?->id],
+            'latitude' => ['required', 'numeric', 'min:-90', 'max:90'],
+            'longitude' => ['required', 'numeric', 'min:-180', 'max:180'],
+            'marker_color' => ['required', 'hex_color'],
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+}
