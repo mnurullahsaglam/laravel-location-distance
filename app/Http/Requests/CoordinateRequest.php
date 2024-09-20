28624e7abcd3302f5b2b4f03673322c5dd3a1b55
mnurullahsaglam <nurullahsl87@gmail.com>
2024-09-20 06:38:13 +0300

    create form request class for coordinates

---
+++ app/Http/Requests/CoordinateRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CoordinateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'latitude' => ['required', 'numeric', 'min:-90', 'max:90'],
+            'longitude' => ['required', 'numeric', 'min:-180', 'max:180'],
+        ];
+    }
+}
