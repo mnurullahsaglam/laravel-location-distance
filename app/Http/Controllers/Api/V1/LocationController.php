7b9c122063ad72294fedfabb63bf59d63238931f
mnurullahsaglam <nurullahsl87@gmail.com>
2024-09-20 06:22:11 +0300

    add cache and method return types

--- app/Http/Controllers/Api/V1/LocationController.php
+++ app/Http/Controllers/Api/V1/LocationController.php
@@ -6,34 +6,39 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LocationRequest;
 use App\Http\Resources\LocationResource;
 use App\Models\Location;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cache;
 
 class LocationController extends Controller
 {
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        return LocationResource::collection(Location::all());
+        return LocationResource::collection(Cache::rememberForever('locations', function () {
+            return Location::all();
+        }));
     }
 
-    public function store(LocationRequest $request)
+    public function store(LocationRequest $request): LocationResource
     {
         $location = Location::create($request->validated());
 
         return new LocationResource($location);
     }
 
-    public function show(Location $location)
+    public function show(Location $location): LocationResource
     {
         return new LocationResource($location);
     }
 
-    public function update(LocationRequest $request, Location $location)
+    public function update(LocationRequest $request, Location $location): LocationResource
     {
         $location->update($request->validated());
 
         return new LocationResource($location);
     }
 
-    public function destroy(Location $location)
+    public function destroy(Location $location): Response
     {
         $location->delete();
 
