ac5e2639434f618fac06b9f785938baad99ccd07
mnurullahsaglam <nurullahsl87@gmail.com>
2024-09-20 05:15:26 +0300

    add return types of methods in LocationController

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -4,46 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LocationRequest;
 use App\Models\Location;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class LocationController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         $locations = Location::orderBy('name')->paginate();
 
         return view('locations.index', compact('locations'));
     }
 
-    public function create()
+    public function create(): View
     {
         return view('locations.create');
     }
 
-    public function store(LocationRequest $request)
+    public function store(LocationRequest $request): RedirectResponse
     {
         Location::create($request->validated());
 
         return to_route('locations.index');
     }
 
-    public function show(Location $location)
+    public function show(Location $location): View
     {
         return view('locations.show', compact('location'));
     }
 
-    public function edit(Location $location)
+    public function edit(Location $location): View
     {
         return view('locations.edit', compact('location'));
     }
 
-    public function update(LocationRequest $request, Location $location)
+    public function update(LocationRequest $request, Location $location): RedirectResponse
     {
         $location->update($request->validated());
 
         return to_route('locations.index');
     }
 
-    public function destroy(Location $location)
+    public function destroy(Location $location): RedirectResponse
     {
         $location->delete();
 
