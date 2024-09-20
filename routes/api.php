57c37d447451114307845d47d42120b5c3e48793
mnurullahsaglam <nurullahsl87@gmail.com>
2024-09-20 06:38:46 +0300

    add createRoute route to api

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Api\V1\LocationController;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::apiResource('/locations', LocationController::class);
+    Route::post('create-route', [LocationController::class, 'createRoute']);
 });
